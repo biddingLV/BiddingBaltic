757b6326ae339cc3a2c353c88bca4e2749aa6257
kerpekri <kristaps.kerpe@gmail.com>
2019-01-26 12:21:33 +0100

    remove server side pre-rendering

--- Bidding/Startup.cs
+++ Bidding/Startup.cs
@@ -143,8 +143,8 @@ namespace Bidding
 
                 if (env.IsDevelopment())
                 {
-                    // spa.UseAngularCliServer(npmScript: "start");
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                    spa.UseAngularCliServer(npmScript: "start");
+                    //spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
