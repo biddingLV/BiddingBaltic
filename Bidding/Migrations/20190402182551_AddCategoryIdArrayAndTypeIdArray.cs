2891e0d691a623fa7eb14833d2338cfbcca12cff
kerpekri <kristaps.kerpe@gmail.com>
2019-04-02 20:39:36 +0200

    add migrations for stored procedures and types as tables

---
+++ Bidding/Migrations/20190402182551_AddCategoryIdArrayAndTypeIdArray.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bidding.Migrations
+{
+    public partial class AddCategoryIdArrayAndTypeIdArray : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"
+                CREATE TYPE [CategoryIdArray] AS TABLE ([CategoryId] INT NULL);
+                CREATE TYPE[TypeIdArray] AS TABLE([TypeId] INT NULL);
+            ";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"DROP PROCEDURE GetSubCategoriesWithCount;";
+
+            migrationBuilder.Sql(sp);
+        }
+    }
+}
