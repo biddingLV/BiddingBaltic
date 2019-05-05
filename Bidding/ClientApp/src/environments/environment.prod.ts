0216f0c07b83d7306a20f8c941582b2ebb49ca59
kerpekri <kristaps.kerpe@gmail.com>
2019-05-05 16:47:53 +0200

    trying to fix prod build

--- Bidding/ClientApp/src/environments/environment.prod.ts
+++ Bidding/ClientApp/src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   production: true,
   authClientID: 'QKrbvTxwlaDq6qhfAtqHoYL3g6KmYccZ',
   authDomain: 'biddinglv.eu.auth0.com',
-  authAudience: 'http://biddinglv-dev.azurewebsites.net',
-  authRedirectUri: 'http://biddinglv-dev.azurewebsites.net',
-  authLoginUri: 'http://biddinglv-dev.azurewebsites.net/start/auth/login',
-  authLogoutUri: 'http://biddinglv-dev.azurewebsites.net/start/auth/logout'
+  authAudience: 'http://biddinglv.azurewebsites.net',
+  authRedirectUri: 'http://biddinglv.azurewebsites.net',
+  authLoginUri: 'http://biddinglv.azurewebsites.net/start/auth/login',
+  authLogoutUri: 'http://biddinglv.azurewebsites.net/start/auth/logout'
 };
