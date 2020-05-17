056513c41b25f74ac71bcc80312f3959f4bdb3d2
Kristaps Kerpe <Kristaps.Kerpe@atea.com>
2020-05-17 11:21:56 +0300

    update homepage header with newest styles

--- Bidding/ClientApp/src/app/home/components/header/header.component.ts
+++ Bidding/ClientApp/src/app/home/components/header/header.component.ts
@@ -1,41 +1,13 @@
 // angular
 import { Component, OnInit } from "@angular/core";
 
-// internal
-import { AuthService } from "ClientApp/src/app/core/services/auth/auth.service";
-import { ButtonsService } from "ClientApp/src/app/core/services/buttons/buttons.service";
-import { CustomButtonModel } from "ClientApp/src/app/core/services/buttons/custom-button.model";
-
 @Component({
   selector: "app-home-header",
   templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.scss"]
+  styleUrls: ["./header.component.scss"],
 })
 export class HomeHeaderComponent implements OnInit {
-  // template
-  buttonConfig: CustomButtonModel;
-  isLoggedIn: boolean;
+  constructor() {}
 
-  userDetails = this.authService.userDetails;
-
-  constructor(
-    private authService: AuthService,
-    private buttonsService: ButtonsService
-  ) {}
-
-  ngOnInit(): void {
-    this.isLoggedIn = this.userDetails != null ? true : false;
-    this.handleSignInButton();
-  }
-
-  /** Used to handle sign-in */
-  onSignInChange(event: boolean) {
-    this.authService.login();
-  }
-
-  private handleSignInButton(): void {
-    this.buttonConfig = {
-      ...this.buttonsService.defaultButtonConfig
-    };
-  }
+  ngOnInit(): void {}
 }
